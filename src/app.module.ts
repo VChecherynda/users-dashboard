c1b92b6d6d375c0876803c41189c841b4dbee2f1
vadym <checherinda.vadym@webbylab.com.com>
2023-02-26 20:58:30 +0200

    Add logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { logger } from './middleware/logger.middleware';
 import { UsersModule } from './users/users.module';
+import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [UsersModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(logger).forRoutes(UsersController);
+  }
+}
