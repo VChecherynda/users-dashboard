ae25fdfa24724c4651a62e893291c42473103393
vadym <checherinda.vadym@webbylab.com.com>
2023-02-27 23:23:43 +0200

    Install ConfigeModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { logger } from './middleware/logger.middleware';
 import { UsersModule } from './users/users.module';
 import { UsersController } from './users/users.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ConfigModule.forRoot()],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
