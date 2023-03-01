44a7412702a2b29b37e9c7c72347e25b2b2dd0b4
vadym <checherinda.vadym@webbylab.com.com>
2023-03-01 02:24:43 +0200

    Delete dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './exception/all-exception-filter';
-import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  dotenv.config();
-
   const app = await NestFactory.create(AppModule);
 
   const httpAdapterHost = app.get(HttpAdapterHost);
