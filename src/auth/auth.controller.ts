825149ed84d4afb31b874fd2182487c6222e80e3
vadym <checherinda.vadym@webbylab.com.com>
2023-03-06 23:15:02 +0200

    Add route path

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -15,7 +15,7 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @UseGuards(AuthGuard('local'))
-  @Post('login')
+  @Post('/login')
   async login(@Request() req) {
     if (!req.user) {
       throw new NotFoundException(`User not found`);
@@ -24,12 +24,12 @@ export class AuthController {
     return await this.authService.login(req.user);
   }
 
-  @Post('signup')
+  @Post('/signup')
   async signup(@Body() createUserDto: CreateUserDto) {
     await this.authService.signUpUser(createUserDto);
   }
 
-  @Post('reset-password')
+  @Post('/reset-password')
   async reset() {
     throw new Error('Reset password not implemented');
   }
