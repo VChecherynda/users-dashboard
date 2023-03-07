66f35d7b9dc7b3d7d5e6b7fbff875c47e3405d21
vadym <checherinda.vadym@webbylab.com.com>
2023-03-07 02:39:53 +0200

    Add login schema

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,17 @@
 import {
   Body,
   Controller,
+  HttpCode,
+  HttpStatus,
   NotFoundException,
   Post,
   Request,
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
-import { CreateUserDto } from './dto';
+import { CreateUserDto, LoginUserDto } from './dto';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -17,7 +19,13 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @UseGuards(AuthGuard('local'))
+  @HttpCode(HttpStatus.OK)
   @Post('login')
+  @ApiBody({ type: LoginUserDto })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Login user and return token',
+  })
   async login(@Request() req) {
     if (!req.user) {
       throw new NotFoundException(`User not found`);
--- src/auth/dto/index.ts
+++ src/auth/dto/index.ts
@@ -1,4 +1,5 @@
 import { CreateUserDto } from './create-user.dto';
+import { LoginUserDto } from './login-user.dto';
 import { SavedUserDto } from './saved-user.dto';
 
-export { CreateUserDto, SavedUserDto };
+export { CreateUserDto, SavedUserDto, LoginUserDto };
