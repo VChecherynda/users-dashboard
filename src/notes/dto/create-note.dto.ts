ed0056b326ba7817d39a1e1bb9fdc1397335f355
vadym <checherinda.vadym@webbylab.com.com>
2023-03-15 01:41:36 +0200

    Add create note endpoint

--- src/notes/dto/create-note.dto.ts
+++ src/notes/dto/create-note.dto.ts
@@ -1,1 +1,14 @@
-export class CreateNoteDto {}
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
+
+export class CreateNoteDto {
+  @IsUUID()
+  userId: string;
+
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+}
--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateNoteDto } from './dto/create-note.dto';
 import { UpdateNoteDto } from './dto/update-note.dto';
+import { Note } from './entities/note.entity';
 
 @Injectable()
 export class NotesService {
+  constructor(
+    @InjectRepository(Note)
+    private notesRepository: Repository<Note>,
+  ) {}
+
   create(createNoteDto: CreateNoteDto) {
-    return 'This action adds a new note';
+    return this.notesRepository.save(createNoteDto);
   }
 
   findAll() {
